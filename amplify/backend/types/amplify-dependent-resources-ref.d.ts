93665928a1eb579ee7318bece0bbb4f8c8bff6be
Chris Womack <cpwomack@amazon.com>
2023-09-19 19:45:30 -0600

    STORAGE WORKING

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,8 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "storage": {
+    "s306c34e17": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -10,13 +10,8 @@ Amplify.configure({...awsExports,
     identityPoolId: 'us-west-2:5f5a6b06-3499-4822-bc08-4416fb55dd64',
     // identityPoolId: 'us-west-2:178e63e6-61d8-4a48-a91e-1ff0f06e5118',
 
-
     // (required)- Amazon Cognito Regionamplify 
     region: 'us-west-2',
-
-    // // (optional) - Amazon Cognito Federated Identity Pool Region
-    // // Required only if it's different from Amazon Cognito Region
-    // identityPoolRegion: 'XX-XXXX-X',
 
     // (optional) - Amazon Cognito User Pool ID
     userPoolId: 'us-west-2_QIK1IZFdh',
@@ -24,34 +19,7 @@ Amplify.configure({...awsExports,
     // (optional) - Amazon Cognito Web Client ID (26-char alphanumeric string, App client secret needs to be disabled)
     userPoolWebClientId: 's3n7aqptqag0rrh7k1epf9msb',
 
-    // // (optional) - Enforce user authentication prior to accessing AWS resources or not
-    // mandatorySignIn: false,
-
-    // (optional) - Configuration for cookie storage
-    // Note: if the secure flag is set to true, then the cookie transmission requires a secure protocol
-    // cookieStorage: {
-    //   // - Cookie domain (only required if cookieStorage is provided)
-    //   domain: '.yourdomain.com',
-    //   // (optional) - Cookie path
-    //   path: '/',
-    //   // (optional) - Cookie expiration in days
-    //   expires: 365,
-    //   // (optional) - See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie/SameSite
-    //   sameSite: 'strict' | 'lax',
-    //   // (optional) - Cookie secure flag
-    //   // Either true or false, indicating if the cookie transmission requires a secure protocol (https).
-    //   secure: true
-    // },
     // SIGN ON URL = https://tea-mimic.auth.us-west-2.amazoncognito.com/authorize?client_id=s3n7aqptqag0rrh7k1epf9msb&response_type=code&scope=email+openid&redirect_uri=https%3A%2F%2Fexample.com&identity_provider=azure-tea-mimic
-
-    // // (optional) - customized storage object
-    // storage: MyStorage,
-
-    // // (optional) - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
-    // authenticationFlowType: 'USER_PASSWORD_AUTH',
-
-    // // (optional) - Manually set key value pairs that can be passed to Cognito Lambda Triggers
-    // clientMetadata: { myCustomKey: 'myCustomValue' },
 
     // (optional) - Hosted UI configuration
     oauth: {
@@ -69,96 +37,15 @@ Amplify.configure({...awsExports,
       responseType: 'code' // 'code' or 'token', note that REFRESH token will only be generated when the responseType is code
     }
   },
-  // Storage: {
-  //   AWSS3: {
-  //     bucket: 'samlazuredf0e31194ac548188b25b0059eb825bc', // (required) -  Amazon S3 bucket name
-  //     region: 'us-east-2' // (optional) -  Amazon service region
-  //   }
-  // }
+  Storage: {
+    AWSS3: {
+      bucket: 'samlazuredf0e31194ac548188b25b0059eb825bc', // (required) -  Amazon S3 bucket name
+      region: 'us-west-2' // (optional) -  Amazon service region
+    }
+  }
 });
 
 
-
-// function App() {
-//   const [user, setUser] = useState(null); // State to hold user data
-//   const [loading, setLoading] = useState(true); // State to track loading state
-
-//   useEffect(() => {
-//     Auth.currentAuthenticatedUser()
-//       .then(userData => setUser(userData)) // Set user data if authenticated
-//       .catch(() => setUser(null))
-//       .finally(() => setLoading(false)); // Set loading state when done fetching
-//   }, []);
-
-//   return (
-//     <div className="App">
-//       <h1>Tea Mimic</h1>
-//       {loading ? (
-//         <p>Loading...</p>
-//       ) : (
-//         <>
-//           {user ? ( // If user is authenticated, show user info and sign out button
-//             <div>
-//               <p>Welcome, {user.attributes.email}</p>
-//               <button onClick={() => Auth.signOut()}>Sign Out</button>
-//             </div>
-//           ) : ( // If user is not authenticated, show sign in button
-//             <button onClick={() => Auth.federatedSignIn({ customProvider: "azure-tea-mimic" })}>Sign In</button>
-//           )}
-//         </>
-//       )}
-//     </div>
-//   );
-// }
-
-
-// function App() {
-//   const [user, setUser] = useState(null);
-//   const [loading, setLoading] = useState(true);
-
-//   useEffect(() => {
-//     Auth.currentAuthenticatedUser()
-//       .then(userData => setUser(userData))
-//       .catch(() => setUser(null))
-//       .finally(() => setLoading(false));
-//   }, []);
-
-//   // Function to handle file upload
-//   const handleFileUpload = async () => {
-//     try {
-//       const file = new Blob(['Hello, world!'], { type: 'text/plain' });
-//       await Storage.put('myFile.txt', file, {
-//         contentType: 'text/plain'
-//       });
-//       console.log('File uploaded successfully');
-//     } catch (error) {
-//       console.error('Error uploading file:', error);
-//     }
-//   };
-
-//   return (
-//     <div className="App">
-//       <h1>Tea Mimic</h1>
-//       {loading ? (
-//         <p>Loading...</p>
-//       ) : (
-//         <>
-//           {user ? (
-//             <div>
-//               <p>Welcome, {user.attributes.email}</p>
-//               <button onClick={() => Auth.signOut()}>Sign Out</button>
-//               <button onClick={handleFileUpload}>Upload File</button> {/* Add the button to trigger file upload */}
-//             </div>
-//           ) : (
-//             <button onClick={() => Auth.federatedSignIn({ customProvider: "azure-tea-mimic" })}>Sign In</button>
-//           )}
-//         </>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default App;
 
 function App() {
   const [user, setUser] = useState(null);
