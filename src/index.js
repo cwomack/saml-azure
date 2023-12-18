5a84a830a63072db5cfc17e6555564a6c55fe930
Chris Womack <cpwomack@amazon.com>
2023-12-18 14:46:32 -0700

    notes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// final configuration (in v5)
+// Scoped Congig for Auth and Storage
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
