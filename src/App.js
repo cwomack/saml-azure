6abedc3ea0ee1944e8ecdea4f3de583661a4b4fc
Chris Womack <cpwomack@amazon.com>
2024-01-18 16:57:06 -0700

    last comment updates

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { StorageManager } from '@aws-amplify/ui-react-storage';
 import '@aws-amplify/ui-react/styles.css';
 import awsExports from './aws-exports';
 
-// FINAL configuration (in v5)
+// FINAL working configuration (in v5)
 
 Amplify.configure({...awsExports,
   Auth: {
